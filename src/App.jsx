f05b2b0d7a1b80ad527c23f7243ec741b049faa6
Dariusz Okoński <darek200180@gmail.com>
2024-04-16 21:12:58 +0200

    add people

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,51 @@
+import { useState } from 'react';
+import {
+  FaChevronCircleLeft,
+  FaChevronCircleRight,
+  FaQuoteRight,
+} from 'react-icons/fa';
+
+import people from './data';
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { id, image, job, name, text } = people[index];
+
+  const nextPerson = () => {
+    if (index < people.length - 1) {
+      setIndex((currentIndex) => currentIndex + 1);
+    }
+  };
+  const prevPerson = () => {
+    if (index > 0) {
+      setIndex((currentIndex) => currentIndex - 1);
+    }
+  };
+
+  return (
+    <main>
+      <article className='review'>
+        <div className='img-container'>
+          <img src={image} alt={name} className='person-img' />
+          <span className='quote-icon'>
+            <FaQuoteRight />
+          </span>
+        </div>
+
+        <h4 className='author'>{name}</h4>
+        <p className='job'>{job}</p>
+        <p className='info'>{text}</p>
+
+        <div className='btn-container'>
+          <button className='prev-btn' onClick={prevPerson}>
+            <FaChevronCircleLeft />
+          </button>
+          <button className='next-btn' onClick={nextPerson}>
+            <FaChevronCircleRight />
+          </button>
+        </div>
+      </article>
+    </main>
+  );
 };
 export default App;
